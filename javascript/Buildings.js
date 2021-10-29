94336da441e6827af40fa3cddb9616294a85ab42
Matt McNaney <mcnaneym@appstate.edu>
2021-10-29 08:13:52 -0400

    Added UREC building

--- javascript/Buildings.js
+++ javascript/Buildings.js
@@ -1,10 +1,14 @@
 const Buildings = [
+  {name: 'B.B. Dougherty', address: '438 Academy Street, Boone, NC 28608'},
   {name: 'John E. Thomas (JET)', address: '287 Rivers Street, Boone, NC 28608'},
   {
     name: 'Plemmons Student Union',
     address: '263 Locust Street, Boone, NC 28608',
   },
-  {name: 'B.B. Dougherty', address: '438 Academy Street, Boone, NC 28608'},
+  {
+    name: 'Student Recreation Center',
+    address: '150 Bodenheimer Drive, Boone, NC 28608',
+  },
 ]
 
 export default Buildings
