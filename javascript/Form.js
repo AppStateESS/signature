d51815c889a8a1bf457c6c40d932b035c432f25e
Matt McNaney <mcnaneym@appstate.edu>
2021-11-01 11:54:10 -0400

    Bootstrap styling added.
    Layout clean up.

--- javascript/Form.js
+++ javascript/Form.js
@@ -12,12 +12,13 @@ const buildingOptions = Buildings.map((value, key) => {
 })
 const Form = ({info, update}) => {
   return (
-    <table>
+    <table className="table table-striped">
       <tbody>
         <tr>
           <td>Name</td>
           <td>
             <input
+              className="form-control"
               type="text"
               onChange={(e) => update('name', e.target.value)}
               value={info.name}
@@ -28,6 +29,7 @@ const Form = ({info, update}) => {
           <td>Position</td>
           <td>
             <input
+              className="form-control"
               type="text"
               onChange={(e) => update('position', e.target.value)}
               value={info.position}
@@ -37,7 +39,8 @@ const Form = ({info, update}) => {
         <tr>
           <td>Department</td>
           <td>
-            <input
+            <textarea
+              className="form-control"
               type="text"
               onChange={(e) => update('department', e.target.value)}
               value={info.department}
@@ -47,45 +50,62 @@ const Form = ({info, update}) => {
         <tr>
           <td>Room/Suite</td>
           <td>
-            <select onChange={(e) => update('roomType', e.target.value)}>
-              <option>Room</option>
-              <option>Suite</option>
-            </select>
-            <input
-              type="text"
-              onChange={(e) => update('room', e.target.value)}
-              value={info.room}
-            />
+            <div className="row">
+              <div className="col-sm-5">
+                <select
+                  onChange={(e) => update('roomType', e.target.value)}
+                  className="form-control">
+                  <option>Room</option>
+                  <option>Suite</option>
+                </select>
+              </div>
+              <div className="col-sm-7">
+                <input
+                  className="form-control"
+                  type="text"
+                  size="5"
+                  onChange={(e) => update('room', e.target.value)}
+                  value={info.room}
+                />
+              </div>
+            </div>
           </td>
         </tr>
         <tr>
           <td>Email</td>
           <td>
-            <input
-              type="text"
-              onChange={(e) => update('email', e.target.value)}
-              value={info.email}
-            />
-            @appstate.edu
+            <div className="input-group mb-3">
+              <input
+                className="form-control"
+                type="text"
+                onChange={(e) => update('email', e.target.value)}
+                value={info.email}
+              />
+              <span className="input-group-text">@appstate.edu</span>
+            </div>
           </td>
         </tr>
         <tr>
           <td>Phone</td>
           <td>
-            828-262-
-            <input
-              type="text"
-              size="4"
-              maxLength="4"
-              onChange={(e) => update('phone', e.target.value)}
-              value={info.phone}
-            />
+            <div className="input-group mb-3">
+              <span className="input-group-text">828-262-</span>
+              <input
+                className="form-control"
+                type="text"
+                size="4"
+                maxLength="4"
+                onChange={(e) => update('phone', e.target.value)}
+                value={info.phone}
+              />
+            </div>
           </td>
         </tr>
         <tr>
           <td>Building</td>
           <td>
             <select
+              className="form-control"
               onChange={(e) => {
                 update('building', Buildings[e.target.value].name)
                 update('address', Buildings[e.target.value].address)
@@ -98,6 +118,7 @@ const Form = ({info, update}) => {
           <td>AppState Box</td>
           <td>
             <input
+              className="form-control"
               size="6"
               type="text"
               value={info.box}
@@ -109,6 +130,7 @@ const Form = ({info, update}) => {
           <td>Address</td>
           <td>
             <input
+              className="form-control"
               type="text"
               onChange={(e) => update('address', e.target.value)}
               value={info.address}
@@ -119,6 +141,7 @@ const Form = ({info, update}) => {
           <td>Website URL</td>
           <td>
             <input
+              className="form-control"
               type="text"
               onChange={(e) => update('site', e.target.value)}
               value={info.site}
