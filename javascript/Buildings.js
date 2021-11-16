46940519ac117d45261026dcf297eb049d9ca7dc
Matt McNaney <mcnaneym@appstate.edu>
2021-11-16 11:58:40 -0500

    Remove street abbreviations
    Added Thunder Hill and Appalachian Hall.

--- javascript/Buildings.js
+++ javascript/Buildings.js
@@ -1,12 +1,16 @@
 const Buildings = [
+  {
+    name: 'Appalachian Hall',
+    address: '214 Locust Street, Boone, NC 28608',
+  },
   {name: 'B.B. Dougherty', address: '438 Academy Street, Boone, NC 28608'},
   {
     name: 'Child Development Center',
-    address: '538 Popular Grove Rd., Boone, NC 28607',
+    address: '538 Popular Grove Road, Boone, NC 28607',
   },
   {
     name: 'I.G Greer',
-    address: '401 Academy St, Boone, NC 28607',
+    address: '401 Academy Street, Boone, NC 28607',
   },
   {name: 'John E. Thomas (JET)', address: '287 Rivers Street, Boone, NC 28608'},
   {name: 'Miles Annas Building', address: '614 Howard Street, Boone, NC 28608'},
@@ -17,6 +21,10 @@ const Buildings = [
   {
     name: 'Student Recreation Center',
     address: '150 Bodenheimer Drive, Boone, NC 28608',
+  },
+  {
+    name: 'Thunder Hill',
+    address: '228 Stadium Drive, Boone, NC 28608',
   },
 ]
 
